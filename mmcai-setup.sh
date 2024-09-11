c0676992410c5d6e5a08dc40e5c14606974e97c7
Svetly Todorov <svetly.todorov@memverge.com>
2024-09-11 11:51:39 -0700

    mmcai-setup.sh: install internal chart if credentials allow

--- mmcai-setup.sh
+++ mmcai-setup.sh
@@ -27,11 +27,65 @@ chmod +x mysql-pre-setup.sh
 ./mysql-pre-setup.sh
 
 div
-log_good "Creating namespaces if needed..."
-div
+log_good "Creating namespaces, and applying image pull secrets if present..."
+
+install_internal=false
+install_repository="oci://ghcr.io/memverge/charts"
+
+# Log into helm using the credentials from the image pull secret.
+function helm_login() {
+    # Extract creds
+    secret_json=$(
+        kubectl get secret memverge-dockerconfig -n mmcai-system --output="jsonpath={.data.\.dockerconfigjson}" |
+        base64 --decode
+    )
+    secret_user=$(echo ${secret_json} | jq -r '.auths."ghcr.io/memverge".username')
+    secret_token=$(echo ${secret_json} | jq -r '.auths."ghcr.io/memverge".password')
+
+    # Attempt login
+    if helm login ghcr.io/memverge -u $secret_user -p $secret_token; then
+        div
+        log_good "Helm login was successful."
+    else
+        div
+        log_bad "Helm login was unsuccessful. Please provide an mmcai-ghcr-secret.yaml that allows helm login."
+        div
+        log "Report:"
+        cat mmcai-ghcr-secret.yaml
+        div
+        exit 1
+    fi
+}
+
+function determine_install_type() {
+    # mmcai-ghcr-secret may allow user to pull internal charts.
+    # If so, ask user if they want to pull internal or external.
+    helm pull oci://ghcr.io/memverge/charts/internal/mmcai-cluster --devel
+    if ls mmcai-cluster* > /dev/null 2>&1; then
+        rm mmcai-cluster*
+
+        div
+        log_good "Your mmcai-ghcr-secret.yaml allows you to pull internal images."
+        
+        read -p "Would you like to install internal builds on your cluster? [Y/n]:" install_internal
+        case $install_internal in
+            [Nn]* ) install_internal=false;;
+            * ) install_internal=true;;
+        esac
+    fi
+
+    if $install_internal; then
+        install_repository="oci://ghcr.io/memverge/charts/internal"
+    fi
+
+    div
+    log "Based on mmcai-ghcr-secret.yaml credentials, will install from $install_repository"
+}
 
 if [[ -f "mmcai-ghcr-secret.yaml" ]]; then
     kubectl apply -f mmcai-ghcr-secret.yaml
+    helm_login
+    determine_install_type
 else
     kubectl create ns $NAMESPACE
     kubectl create ns mmcloud-operator-system
@@ -42,7 +96,7 @@ fi
 kubectl get namespace monitoring &>/dev/null || kubectl create namespace monitoring
 
 div
-log_good "Creating secrets if needed..."
+log_good "Creating mysql secret..."
 div
 
 ## Create MySQL secret
@@ -55,12 +109,12 @@ kubectl -n $NAMESPACE create secret generic mmai-mysql-secret \
     --from-literal=mysql-replication-password=$MYSQL_ROOT_PASSWORD
 
 div
-log_good "Beginning installation..."
+log_good "Installing charts..."
 div
 
 ## install mmc.ai system
-helm install --debug -n $NAMESPACE mmcai-cluster oci://ghcr.io/memverge/charts/mmcai-cluster \
+helm install --debug -n $NAMESPACE mmcai-cluster ${install_repository}/mmcai-cluster \
     --set billing.database.nodeHostname=$mysql_node_hostname
 
 ## install mmc.ai management
-helm install --debug -n $NAMESPACE mmcai-manager oci://ghcr.io/memverge/charts/mmcai-manager
+helm install --debug -n $NAMESPACE mmcai-manager ${install_repository}/mmcai-manager
